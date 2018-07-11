df4626ab0c7676ed8be554969eabe6b3ad6d94e2
Alison <alisonw30x@gmail.com>
2018-07-11 09:58:27 -0700

    working axios implementation as a fetch alternative

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 
 const DEFAULT_QUERY = '';
@@ -50,6 +51,9 @@ class Button extends Component {
 
 
 class App extends Component {
+  //for tracking lifecycle state
+  _isMounted = false;
+
   constructor(props) {
     super(props);
     this.state = {
@@ -81,16 +85,20 @@ class App extends Component {
   }
 
   fetchSearchTopStories(searchTerm, page=0) {
-  fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
-  .then(response => response.json())
-  .then(result => this.setSearchTopStories(result))
-  .catch(error => this.setState({error}));
+    axios(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
+    .then(result => this._isMounted && this.setSearchTopStories(result.data))
+    .catch(error => this._isMounted && this.setState({error}));
   }
 
   componentDidMount() {
+    this._isMounted = true;
     const { searchTerm } =this.state;
     this.setState({searchKey: searchTerm});
     this.fetchSearchTopStories(searchTerm);
+  }
+
+  componentWillUnMount() {
+    this._isMounted =false;
   }
 
   onDismiss(id){
